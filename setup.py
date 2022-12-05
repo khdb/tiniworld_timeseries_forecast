edf50f1b06805f16342ad0df4de146c4caf354bd
Khoa Do <khoa.do.bui@gmail.com>
2022-12-05 17:20:57 +0100

    added Dockerfile on local and build docker image.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='tiniworld_ts_project',
-      version="1.0",
+setup(name='tiniworld_core',
+      version="0.0.4",
       description="A Time-Series Forecasting Project with Facebook Prophet",
       packages=find_packages(),
       install_requires=requirements,
-      test_suite='tests',
+      #test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/tiniworld_core-run'],
+      #scripts=['scripts/tiniworld_core-run'],
       zip_safe=False)
